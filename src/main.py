c2fb192e433f1e8eb553b75e2cb034bbb1fc1549
Francisco Melero <francisco.melero@politecnicomalaga.com>
2021-01-29 14:14:07 +0100

    jorge 1.0

--- src/main.py
+++ src/main.py
@@ -6,8 +6,8 @@ def suma(a,b):
 
 @app.route("/")
 def hello():
-        res = suma(3,2)
-        return "Hola Jorge, Mario  y Fatima, soy Francisco %s" % (res)
+        res = suma(13,22)
+        return "Hola Jorge, Mario  y Fatima, soy Francisco y la suma de 3 + 2 es  %s" % (res)
 if __name__ == "__main__":
-        app.run(host='0.0.0.0',port=5000)
+        app.run(host='0.0.0.0',port=4000)
 
