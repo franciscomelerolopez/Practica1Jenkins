505c6392ebcfac0211258a783625655735a94ae7
franciscomelerolopez <francisco.melero@politecnicomalaga.com>
2021-01-31 15:50:08 +0100

    HLC-2.0

--- src/main.py
+++ src/main.py
@@ -9,5 +9,5 @@ def hello():
         res = suma(3,2)
         return "Hola Mundo, soy Francisco Melero y la suma de 3 + 2 es  %s" % (res)
 if __name__ == "__main__":
-        app.run(host='0.0.0.0',port=6000)
+        app.run(host='0.0.0.0',port=5000)
 
