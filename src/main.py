5bcc0f9f5bf2facc508131852796ff71b91b8fac
franciscomelerolopez <francisco.melero@politecnicomalaga.com>
2021-01-31 11:55:59 +0100

    Nuevo cambio

--- src/main.py
+++ src/main.py
@@ -6,8 +6,8 @@ def suma(a,b):
 
 @app.route("/")
 def hello():
-        res = suma(13,22)
-        return "Hola Jorge, Mario  y Fatima, soy Francisco y la suma de 3 + 2 es  %s" % (res)
+        res = suma(3,2)
+        return "Hola Mundo, soy Francisco y la suma de 3 + 2 es  %s" % (res)
 if __name__ == "__main__":
         app.run(host='0.0.0.0',port=5000)
 
